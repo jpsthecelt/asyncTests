edba7349b4e51c96cca9769b78ee4da592438d91
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-15 06:56:07 -0700

    Cleaned up asyncTest.py formatting

--- asyncTest.py
+++ asyncTest.py
@@ -17,12 +17,8 @@ async def download_all_sites(sites):
         await asyncio.gather(*tasks, return_exceptions=True)
 
 if __name__ == "__main__":
-    sites = [
-    "https://www.jython.org",
-    "http://olympus.realpython.org/dice",
-    ] * 80
+    sites = [ "https://www.jython.org", "http://olympus.realpython.org/dice", ] * 80
     start_time = time.time()
-#   asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
     asyncio.run(download_all_sites(sites))
     duration = time.time() - start_time
     print(f"Downloaded {len(sites)} sites in {duration} seconds")
